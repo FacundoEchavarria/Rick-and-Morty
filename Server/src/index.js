ddecb78a932dd4ad58e7229b34129001b8c10452
FacundoEchavarria <facundoechavarriap@gmail.com>
2023-07-26 16:44:07 -0300

    Sequelize agragado sin funcion

--- Server/src/index.js
+++ Server/src/index.js
@@ -2,11 +2,16 @@ const express = require('express')
 const server = express()
 const PORT = 3001
 const router = require('./routes/index')
+const {conn} = require('./DB_connection')
 
 server.use(express.json())
-server.listen(PORT, ()=>{
-    console.log('Server listen in port' + PORT);
-})
+
+
+server.listen(PORT, () => {
+conn.sync({force: true})
+console.log('Server listen in port ' + PORT);
+});
+
 server.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Credentials', 'true');
---
+++ Server/src/models/User.js
@@ -0,0 +1,29 @@
+const { DataTypes } = require('sequelize');
+
+module.exports = (sequelize) => {
+   sequelize.define('User', {
+      id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      },
+      email:{
+         type: DataTypes.STRING,
+         allowNull: false,
+         isEmail: true,
+      },
+      password:{
+         type: DataTypes.STRING,
+         allowNull: false
+      },
+      name:{
+         type: DataTypes.STRING,
+         allowNull: false,
+         unique: true,
+      }
+   }, 
+   {
+      timestamps: false 
+   }
+   );
+};
